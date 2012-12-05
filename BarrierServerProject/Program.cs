d0f583b1a4bcca1057e749ede02894a29c8c81de
Partizanes <partizanes@bigmir.net>
2012-12-05 20:27:12 +0300

    [7]add task;check other running programm;logo while loading;

--- BarrierServerProject/Program.cs
+++ BarrierServerProject/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
 using ConsoleFunctions;
 
 namespace BarrierServerProject
@@ -11,16 +14,88 @@ namespace BarrierServerProject
     {
         static void Main(string[] args)
         {
-            int i = 0;
-
-            while (i <= 100)
+            if (System.Diagnostics.Process.GetProcessesByName(Application.ProductName).Length > 1)
             {
-                Code.RenderConsoleProgress(i, '\u2592', ConsoleColor.Green, i.ToString()+ "%");
-                Thread.Sleep(100);
-                i++;
+                WriteLineColor("\n", "Red");
+                WriteLineColor("Приложение уже запущено!", "Red");
+                WriteLineColor("\n", "Red");
+                WriteLineColor("Запускаю завершение работы...", "Red");
+                WriteLineColor("\n", "Red");
+
+                int i = 10;
+
+                while (i != 0)
+                {
+                    Code.RenderConsoleProgress(i * 10, '\u2592', ConsoleColor.Red, "Завершение работы сервера через [" + i.ToString() + " second]");
+                    Thread.Sleep(1000);
+                    i--;
+                }
+                Environment.Exit(0);
             }
-            
+            else
+            {
+                Console.Title = "Barrier_Server";
+
+                Console.Write("\n");
+                WriteLineColor("    Barrier_Server...", "Green");
+                WriteLineColor("            Loading.. ", "Red");
+                WriteLineColor("                      /\\", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                     /  \\", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                    /    \\", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                   /      \\", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                  /        \\", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                 /          \\", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("     The idea and execution  \\", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("          by Part!zanes       \\", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("              /                \\", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("              \\                /", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("               \\              /", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                \\            /", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                 \\          /", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                  \\        /", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                   \\      /", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                    \\    /", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                     \\  /", "Magenta");
+                Thread.Sleep(200);
+                WriteLineColor("                      \\/", "Magenta");
+                WriteLineColor("        Complete ... ", "Green");
+                Console.Write("\n");
+            }
             Console.ReadKey();
+        }
+
+        static void WriteLineColor(string value, string color)
+        {
+            if (color == "Red")
+                Console.ForegroundColor = ConsoleColor.Red;
+            else if (color == "Green")
+                Console.ForegroundColor = ConsoleColor.Green;
+            else if (color == "Magenta")
+                Console.ForegroundColor = ConsoleColor.Magenta;
+            else if (color == "Yellow")
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            else if (color == "Cyan")
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine(value.PadRight(Console.WindowWidth - 1)); // <-- see note
+
+            Console.ResetColor();
         }
     }
 }
