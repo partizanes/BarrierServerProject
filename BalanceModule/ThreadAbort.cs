12f4a9d5b274a68c4b2981471519a69271c976ca
partizanes <partizanes@bigmir.net>
2012-12-14 15:00:13 +0300

    [21]threab abort

---
+++ BalanceModule/ThreadAbort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace BalanceModule
+{
+    public static class ThreadAbort
+    {
+        public static void th_abort()
+        {
+            try { Thread.CurrentThread.Abort(); }
+            
+            catch
+            {
+                Thread.ResetAbort();
+                Server.remove_tr();
+            }
+        }
+    }
+}
