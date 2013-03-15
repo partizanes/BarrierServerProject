7255f077e5e93b4b1c101083270f414919f051fe
partizanes <partizanes@bigmir.net>
2013-03-15 13:22:44 +0300

    [98] Clean

--- PrioritySales/Properties/Resources.Designer.cs
+++ PrioritySales/Properties/Resources.Designer.cs
@@ -89,15 +89,5 @@ namespace PrioritySales.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        public static System.Drawing.Bitmap SettingForm {
-            get {
-                object obj = ResourceManager.GetObject("SettingForm", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
